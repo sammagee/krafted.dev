5623401ee46deae209f1c580df61fcff16c96c41
Sam <s@sammagee.me>
2020-12-18 13:33:58 -0600

    fix logo

--- pages/index.js
+++ pages/index.js
@@ -11,30 +11,29 @@ export default function Home() {
       <main className="flex items-center justify-center h-screen font-sans bg-white">
         <div className="grid w-full max-w-xl grid-cols-1 gap-6 px-8">
           <header className="flex items-center space-x-5 text-6xl font-extrabold">
-            <div className="flex items-center justify-center w-24 h-24 border shadow-xl rounded-2xl bg-gradient-to-br from-gray-700 to-gray-900">
-              <svg className="w-16" viewBox="0 0 1733 1602" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="325.92" y="87.3978" width="956.088" height="956.088" rx="197.453" fill="#EE4444"/>
-                <rect x="325.92" y="87.3978" width="956.088" height="956.088" rx="197.453" fill="url(#paint0_linear)"/>
-                <rect x="325.92" y="87.3978" width="956.088" height="956.088" rx="197.453" stroke="transparent" strokeWidth="173.204"/>
-                <path d="M1293.5 516.071C1217.44 384.795 1027.89 384.795 951.817 516.07L626.286 1077.86C550.009 1209.49 644.991 1374.31 797.129 1374.31H1448.19C1600.33 1374.31 1695.31 1209.49 1619.04 1077.86L1293.5 516.071Z" fill="#3B82F6"/>
-                <path d="M1293.5 516.071C1217.44 384.795 1027.89 384.795 951.817 516.07L626.286 1077.86C550.009 1209.49 644.991 1374.31 797.129 1374.31H1448.19C1600.33 1374.31 1695.31 1209.49 1619.04 1077.86L1293.5 516.071Z" fill="url(#paint1_linear)"/>
-                <path d="M1293.5 516.071C1217.44 384.795 1027.89 384.795 951.817 516.07L626.286 1077.86C550.009 1209.49 644.991 1374.31 797.129 1374.31H1448.19C1600.33 1374.31 1695.31 1209.49 1619.04 1077.86L1293.5 516.071Z" stroke="transparent" strokeWidth="173.204"/>
-                <circle cx="564.942" cy="1036.56" r="478.044" fill="#FACC16"/>
-                <circle cx="564.942" cy="1036.56" r="478.044" fill="url(#paint2_linear)"/>
-                <circle cx="564.942" cy="1036.56" r="478.044" stroke="transparent" strokeWidth="173.204"/>
+            <div className="flex items-center justify-center w-24 h-24 shadow-xl rounded-2xl bg-gradient-to-br from-gray-700 to-gray-900">
+              <svg className="w-16 mt-1 ml-1" viewBox="0 0 1544 1319" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="412" cy="907" r="412" fill="#FACC16"/>
+                <circle cx="412" cy="907" r="412" fill="url(#paint0_linear)"/>
+                <path d="M252 335.5C459 281 610 339 698.5 387C795.5 175 938.5 147.5 1075 175V116C1075 51.935 1023.07 0 959 0H368C303.935 0 252 51.935 252 116V335.5Z" fill="#C4C4C4"/>
+                <path d="M252 335.5C459 281 610 339 698.5 387C795.5 175 938.5 147.5 1075 175V116C1075 51.935 1023.07 0 959 0H368C303.935 0 252 51.935 252 116V335.5Z" fill="#EE4444"/>
+                <path d="M252 335.5C459 281 610 339 698.5 387C795.5 175 938.5 147.5 1075 175V116C1075 51.935 1023.07 0 959 0H368C303.935 0 252 51.935 252 116V335.5Z" fill="url(#paint1_linear)"/>
+                <path d="M1443.08 997.84L1099.3 404.59C1054.63 327.495 943.32 327.464 898.602 404.534L843.5 499.5C843.5 499.5 1109 740 944 1172H1342.75C1432.12 1172 1487.89 1075.17 1443.08 997.84Z" fill="#3B82F6"/>
+                <path d="M1443.08 997.84L1099.3 404.59C1054.63 327.495 943.32 327.464 898.602 404.534L843.5 499.5C843.5 499.5 1109 740 944 1172H1342.75C1432.12 1172 1487.89 1075.17 1443.08 997.84Z" fill="url(#paint2_linear)"/>
+                <path d="M1443.08 997.84L1099.3 404.59C1054.63 327.495 943.32 327.464 898.602 404.534L843.5 499.5C843.5 499.5 1109 740 944 1172H1342.75C1432.12 1172 1487.89 1075.17 1443.08 997.84Z" stroke="black"/>
                 <defs>
-                <linearGradient id="paint0_linear" x1="412.522" y1="174" x2="1195.41" y2="956.884" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#ED4897"/>
-                  <stop offset="1" stopColor="#F43F5F"/>
-                </linearGradient>
-                <linearGradient id="paint1_linear" x1="604.779" y1="393.97" x2="1640.13" y2="1288.19" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#3ABBF8"/>
-                  <stop offset="1" stopColor="#6367F1"/>
-                </linearGradient>
-                <linearGradient id="paint2_linear" x1="173.5" y1="645.116" x2="956.384" y2="1428" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#F3E582"/>
-                  <stop offset="1" stopColor="#F97217"/>
-                </linearGradient>
+                  <linearGradient id="paint0_linear" x1="0" y1="495" x2="824" y2="1319" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#F3E582"/>
+                    <stop offset="1" stop-color="#F97217"/>
+                  </linearGradient>
+                  <linearGradient id="paint1_linear" x1="252" y1="0" x2="330.877" y2="839.326" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#ED4897"/>
+                    <stop offset="1" stop-color="#F43F5F"/>
+                  </linearGradient>
+                  <linearGradient id="paint2_linear" x1="843.5" y1="231.5" x2="1778.09" y2="750.272" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#3ABBF8"/>
+                    <stop offset="1" stop-color="#6367F1"/>
+                  </linearGradient>
                 </defs>
               </svg>
             </div>
